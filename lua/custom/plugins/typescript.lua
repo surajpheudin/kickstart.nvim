580fa6bb3d9d637f93702f800d91615a78ec4038
surajpheudin <pheudinsuraj@gmail.com>
2025-08-08 15:48:52 +0545

    feat: add keymap for organizing imports on typescript propject

--- lua/custom/plugins/typescript.lua
+++ lua/custom/plugins/typescript.lua
@@ -1,7 +1,13 @@
 return {
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {
+    on_attach = function(_, bufnr)
+      local opts = { buffer = bufnr, silent = true }
+
+      -- Organize imports
+      vim.keymap.set('n', '<leader>oi', '<cmd>TSToolsOrganizeImports<CR>', opts)
+    end,
   },
 }
+
