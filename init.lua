a586cafb97d1bb55745f1b65b9c2260e70ea4365
Suraj Pheudin <pheudinsuraj@gmail.com>
2025-09-17 20:29:19 +0545

    feat: add tailwindcss language server for intelligence

--- init.lua
+++ init.lua
@@ -684,6 +684,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        tailwindcss = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
