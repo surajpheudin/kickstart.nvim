34ce32d8185a565269b30e91a07b930201b35898
Suraj Pheudin <pheudinsuraj@gmail.com>
2026-01-17 07:56:56 +0545

    chore: update theme

--- init.lua
+++ init.lua
@@ -911,7 +911,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.o.background = 'light'
+      vim.cmd.colorscheme 'default'
     end,
   },
 
